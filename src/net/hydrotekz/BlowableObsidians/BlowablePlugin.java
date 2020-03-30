5cb71a42046876286590595578c8c3b64d3c9f5c
André Mathisen <waremanu@gmail.com>
2020-03-30 16:46:45 +0200

    Config reload
    Fixes #9

--- src/net/hydrotekz/BlowableObsidians/BlowablePlugin.java
+++ src/net/hydrotekz/BlowableObsidians/BlowablePlugin.java
@@ -1,17 +1,28 @@
 package net.hydrotekz.BlowableObsidians;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.google.common.collect.Lists;
 
 import net.hydrotekz.BlowableObsidians.handlers.*;
 import net.hydrotekz.BlowableObsidians.listeners.*;
 
 public class BlowablePlugin extends JavaPlugin {
-	
+
 	public static BlowablePlugin instance;
 
 	public FileConfiguration config;
-	public int configVersion = 8;
+	public int configVersion = 9;
 	public static int mc_version;
 	public static String mc_protocol;
 
@@ -29,6 +40,7 @@ public class BlowablePlugin extends JavaPlugin {
 		loadListeners();
 		getDataFolder().mkdir();
 		loadConfig();
+		registerCommands();
 	}
 
 	public void loadListeners() {
@@ -40,5 +52,39 @@ public class BlowablePlugin extends JavaPlugin {
 		config.options().copyDefaults(true);
 		config.addDefault("Config Version", 0);
 		Handler.exportConfig();
+	}
+
+	private void registerCommands() {
+		registerCommand("blowable", new ReloadCommand(), "blowableobsidian", "blowableobsidians");
+	}
+
+	public void registerCommand(String name, CommandExecutor executor, String... aliases) {
+		try {
+			Constructor<PluginCommand> constructor = PluginCommand.class.getDeclaredConstructor(String.class, Plugin.class);
+			constructor.setAccessible(true);
+
+			PluginCommand command = constructor.newInstance(name, this);
+
+			command.setExecutor(executor);
+			command.setAliases(Lists.newArrayList(aliases));
+			if (executor instanceof TabCompleter) {
+				command.setTabCompleter((TabCompleter) executor);
+			}
+			this.getCommandMap().register("blowableobsidians", command);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private CommandMap getCommandMap() {
+		try {
+			org.bukkit.Server server = Bukkit.getServer();
+			Field commandMap = server.getClass().getDeclaredField("commandMap");
+			commandMap.setAccessible(true);
+			return (CommandMap) commandMap.get(server);
+		} catch (IllegalAccessException | NoSuchFieldException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
---
+++ src/net/hydrotekz/BlowableObsidians/handlers/ReloadCommand.java
@@ -0,0 +1,42 @@
+package net.hydrotekz.BlowableObsidians.handlers;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.hydrotekz.BlowableObsidians.BlowablePlugin;
+
+public class ReloadCommand implements CommandExecutor {
+
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+		if (!(sender instanceof Player) || sender.hasPermission("blowable.reload")){
+
+			if (args.length == 1) {
+
+				BlowablePlugin.instance.reloadConfig();
+				sender.sendMessage(ChatColor.GREEN + "Config successfully reloaded!");
+
+			}
+
+			else {
+
+				sender.sendMessage(ChatColor.GRAY + "Usage: " + ChatColor.YELLOW + "/blowable reload");
+
+			}
+
+		}
+		else {
+
+			sender.sendMessage(ChatColor.RED + "You don't have permission to this!");
+
+		}
+
+		return true;
+	}
+
+}
